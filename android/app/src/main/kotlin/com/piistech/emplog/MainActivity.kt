db7409fa886a9d60a09b54c9e16ce90ec313917b
Omar Khaium Chowdhury <omar.piistech@gmail.com>
2021-03-05 10:47:06 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/piistech/emplog/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.piistech.emplog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:emplog/src/view/route/route_launcher.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(LauncherRoute());
+}
